c5b34329d3b65ec547e1077a0379d20b57927910
Put your Name in Here <your@email.here>
2023-01-09 23:58:41 +0300

    proje okey

--- client/src/Filmler/KaydedilenlerListesi.js
+++ client/src/Filmler/KaydedilenlerListesi.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React from "react";
+import { useHistory, Link } from "react-router-dom";
 
 export default function KaydedilenlerListesi(props) {
+  const history = useHistory();
+  const handleClick = () => {
+    history.push("/");
+  };
+
   return (
     <div className="saved-list">
       <h3>Kaydedilen Filmler:</h3>
-      {props.list.map(movie => (
-        <span className="saved-movie">{movie.title}</span>
+      {props.list.map((movie) => (
+        <Link to={`/filmler/${movie.id}`}>
+          <span className="saved-movie">{movie.title}</span>
+        </Link>
       ))}
-      <div className="home-button">Anasayfa</div>
+      <div onClick={handleClick} className="home-button">
+        Anasayfa
+      </div>
     </div>
   );
 }
